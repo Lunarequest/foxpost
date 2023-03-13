631a2f1a4eff01be97652dfc42d98f4e3cbc5810
Luna <luna.dragon@suse.com>
2023-03-13 14:38:13 +0530

    wip: ui-rewrite

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '../templates/*.html.tera',
+    '../static/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
