95b51c28ace266e1c7254626feb7396c8874efd6
Luna <luna.dragon@suse.com>
2023-03-15 07:36:41 +0530

    minor tweaks to make navbar work

--- static/js/theme.js
+++ static/js/theme.js
@@ -3,3 +3,10 @@ window.onload = function () {
     let year = new Date().getFullYear();
     copyright.innerText = `${year} Luna Dragon`;
 }
+
+const button = document.querySelector('#menu-button');
+const menu = document.querySelector('#menu');
+
+button.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+});
