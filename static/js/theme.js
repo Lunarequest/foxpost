f2fe420ca008190165f430b3568cdc22734f81e6
Luna <luna.dragon@suse.com>
2023-04-19 11:14:25 +0530

    toggle dark mode

--- static/js/theme.js
+++ static/js/theme.js
@@ -11,6 +11,38 @@ button.addEventListener('click', () => {
 	menu.classList.toggle('hidden');
 });
 
+const icon = document.getElementById("darkmode_icon");
+if (localStorage.theme === 'light' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: light)').matches)) {
+	document.documentElement.classList.remove('dark');
+	icon.className = "fa-solid fa-moon";
+} else {
+	document.documentElement.classList.add('dark');
+	icon.className = "fa-solid fa-sun";
+}
+
+const toggle = document.getElementById("dark-mode-toggle");
+
+function setTheme(mode) {
+	if (mode === "dark") {
+		document.documentElement.classList.add('dark');
+		icon.classList.add('fa-sun');
+		icon.classList.remove('fa-moon');
+	} else if (mode === "light") {
+		document.documentElement.classList.remove('dark');
+		icon.classList.add('fa-moon');
+		icon.classList.remove('fa-sun');
+	}
+	localStorage.setItem('theme', mode)
+}
+
+toggle.addEventListener('click', () => {
+	console.log('click');
+	if (icon.classList.contains('fa-moon')) {
+		setTheme("dark");
+	} else if (icon.classList.contains('fa-sun')) {
+		setTheme("light");
+	}
+});
 
 function share_on_mastodon(_e) {
 	content = encodeURIComponent(document.getElementById("content").value);
