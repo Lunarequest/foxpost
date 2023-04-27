8b2cb086243ea45cb1a87cc125800ea0596d456e
Luna <luna.dragon@suse.com>
2023-04-27 08:47:42 +0530

    use rome for linting

--- static_src/src/js/editor.ts
+++ static_src/src/js/editor.ts
@@ -26,94 +26,98 @@ async function getData(url: string): Promise<string> {
 	});
 }
 
-const easyMDE = new EasyMDE({
-	element: document.getElementById("editor")!,
-	autosave: {
-		enabled: true,
-		delay: 1000,
-		uniqueId: "blog-post",
-	},
-	indentWithTabs: true,
-	renderingConfig: {
-		codeSyntaxHighlighting: true,
-	},
-	tabSize: 4,
-});
+const editorelem = document.getElementById("editor");
 
-getData(`/api/posts/${sessionStorage.getItem("slug")}`).then((data) => {
-	const content = data;
-	easyMDE.value(content);
-});
+if (editorelem) {
+	const easyMDE = new EasyMDE({
+		element: editorelem,
+		autosave: {
+			enabled: true,
+			delay: 1000,
+			uniqueId: "blog-post",
+		},
+		indentWithTabs: true,
+		renderingConfig: {
+			codeSyntaxHighlighting: true,
+		},
+		tabSize: 4,
+	});
 
-const editor = document.getElementById("editor_form") as HTMLFormElement;
-editor.addEventListener("submit", function (e) {
-	update_or_new_post(e);
-});
+	getData(`/api/posts/${sessionStorage.getItem("slug")}`).then((data) => {
+		const content = data;
+		easyMDE.value(content);
+	});
 
-async function update_or_new_post(e: Event) {
-	e.preventDefault();
-	const btn_submit = document.getElementById("submit") as HTMLButtonElement;
-	btn_submit.disabled = true;
-	setTimeout(() => (btn_submit.disabled = false), 2000);
-	const form = new FormData(editor);
-	const json = {
-		title: form.get("title"),
-		description: form.get("description"),
-		tags: form.get("tags"),
-		draft: form.get("draft"),
-		content: form.get("content"),
-	} as PostForm;
+	const editor = document.getElementById("editor_form") as HTMLFormElement;
+	editor.addEventListener("submit", function (e) {
+		update_or_new_post(e);
+	});
 
-	if (json.draft === "not_draft") {
-		json.draft = false;
-	} else {
-		json.draft = true;
-	}
-	if (sessionStorage.getItem("slug")) {
-		const slug = sessionStorage.getItem("slug") as string;
-		json.slug = slug;
-		console.log(json);
-		fetch(`/api/posts/${slug}/update`, {
-			method: "POST",
-			mode: "same-origin",
-			cache: "no-cache",
-			credentials: "same-origin",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(json),
-		})
-			.then(async function (response) {
-				return await response.json();
+	async function update_or_new_post(e: Event) {
+		e.preventDefault();
+		const btn_submit = document.getElementById("submit") as HTMLButtonElement;
+		btn_submit.disabled = true;
+		setTimeout(() => (btn_submit.disabled = false), 2000);
+		const form = new FormData(editor);
+		const json = {
+			title: form.get("title"),
+			description: form.get("description"),
+			tags: form.get("tags"),
+			draft: form.get("draft"),
+			content: form.get("content"),
+		} as PostForm;
+
+		if (json.draft === "not_draft") {
+			json.draft = false;
+		} else {
+			json.draft = true;
+		}
+		if (sessionStorage.getItem("slug")) {
+			const slug = sessionStorage.getItem("slug") as string;
+			json.slug = slug;
+			console.log(json);
+			fetch(`/api/posts/${slug}/update`, {
+				method: "POST",
+				mode: "same-origin",
+				cache: "no-cache",
+				credentials: "same-origin",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(json),
 			})
-			.then(function (resp) {
-				if ("Errors" in resp) {
-					alert(resp.Errors);
-				} else {
-					alert("post has been successfully updated");
-				}
-			});
-	} else {
-		fetch("/api/posts/new", {
-			method: "POST",
-			mode: "same-origin",
-			cache: "no-cache",
-			credentials: "same-origin",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(json),
-		})
-			.then(async function (response) {
-				return await response.json();
+				.then(async function (response) {
+					return await response.json();
+				})
+				.then(function (resp) {
+					if ("Errors" in resp) {
+						alert(resp.Errors);
+					} else {
+						alert("post has been successfully updated");
+					}
+				});
+		} else {
+			fetch("/api/posts/new", {
+				method: "POST",
+				mode: "same-origin",
+				cache: "no-cache",
+				credentials: "same-origin",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(json),
 			})
-			.then(function (resp) {
-				if ("slug" in resp) {
-					sessionStorage.setItem("slug", resp.slug);
-					alert("post has been created");
-				} else {
-					alert(resp.Errors);
-				}
-			});
+				.then(async function (response) {
+					return await response.json();
+				})
+				.then(function (resp) {
+					if ("slug" in resp) {
+						sessionStorage.setItem("slug", resp.slug);
+						alert("post has been created");
+					} else {
+						alert(resp.Errors);
+					}
+				});
+		}
 	}
 }
--- static_src/src/js/search.ts
+++ static_src/src/js/search.ts
@@ -1,109 +1,120 @@
-import Mark from 'mark.js'
-import { Document, type EnrichedDocumentSearchResultSetUnit } from 'flexsearch'
-const searchDataURL = '/api/posts/json'
+import Mark from "mark.js";
+import { Document, type EnrichedDocumentSearchResultSetUnit } from "flexsearch";
+const searchDataURL = "/api/posts/json";
 
 declare global {
-  // take from src/posts/json.rs
-  interface Posts {
-    id: number
-    href: string
-    title: string
-    date: string
-    body: string | null
-  }
+	// take from src/posts/json.rs
+	interface Posts {
+		id: number;
+		href: string;
+		title: string;
+		date: string;
+		body: string | null;
+	}
 }
 
-function init () {
-  console.log('init')
-  const searchBox = document.querySelector('#searchBox')
-  if (searchBox === null) {
-    return
-  }
-  const index = new Document<Posts, ['title', 'href', 'body']>({
-    tokenize: 'reverse',
-    document: {
-      id: 'id',
-      index: ['title', 'body'],
-      store: ['title', 'href', 'body']
-    }
-  })
+function init() {
+	console.log("init");
+	const searchBox = document.querySelector("#searchBox");
+	if (searchBox === null) {
+		return;
+	}
+	const index = new Document<Posts, ["title", "href", "body"]>({
+		tokenize: "reverse",
+		document: {
+			id: "id",
+			index: ["title", "body"],
+			store: ["title", "href", "body"],
+		},
+	});
 
-  fetch(searchDataURL)
-    .then(async pages => await pages.json())
-    .then((pages: Posts[]) => {
-      for (let i = 0; i < pages.length; i++) {
-        index.add(i, pages[i])
-      }
-    })
+	fetch(searchDataURL)
+		.then(async (pages) => await pages.json())
+		.then((pages: Posts[]) => {
+			for (let i = 0; i < pages.length; i++) {
+				index.add(i, pages[i]);
+			}
+		});
 
-  searchBox.addEventListener('keyup', function (event) {
-    const eventtarget = event.currentTarget as HTMLInputElement
-    const searchResultsArea = document.getElementById('searchResults')!
-    const query = eventtarget.value
+	searchBox.addEventListener("keyup", function (event) {
+		const eventtarget = event.currentTarget as HTMLInputElement;
+		const searchResultsArea = document.getElementById("searchResults");
+		const query = eventtarget.value;
+		if (searchResultsArea) {
+			// Only trigger a search when 2 chars. at least have been provided
+			if (query.length < 2) {
+				searchResultsArea.style.display = "none";
+				return;
+			}
 
-    // Only trigger a search when 2 chars. at least have been provided
-    if (query.length < 2) {
-      searchResultsArea.style.display = 'none'
-      return
-    }
-
-    // Display search results
-    const results = index.search(query, 10, { enrich: true })
-    renderResults(results)
-    searchResultsArea.style.display = 'block'
-  })
+			// Display search results
+			const results = index.search(query, 10, { enrich: true });
+			renderResults(results);
+			searchResultsArea.style.display = "block";
+		}
+	});
 }
 
-function renderResults (results: Array<EnrichedDocumentSearchResultSetUnit<Posts>>) {
-  const searchResults = document.querySelector('#searchResults')!
-  const querybox = document.getElementById('searchBox')! as HTMLInputElement
-  const query = querybox.value
-  const BODY_LENGTH = 100
+function renderResults(
+	results: Array<EnrichedDocumentSearchResultSetUnit<Posts>>,
+) {
+	const searchResults = document.getElementById("searchResults");
+	const querybox = document.getElementById("searchBox") as HTMLInputElement;
+	const query = querybox.value;
+	const BODY_LENGTH = 100;
 
-  // Clear search result
-  while (searchResults.firstChild != null) { searchResults.removeChild(searchResults.firstChild) }
+	if (searchResults) {
+		// Clear search result
+		while (searchResults.firstChild != null) {
+			searchResults.removeChild(searchResults.firstChild);
+		}
 
-  // Show message when results is empty
-  if (results.length === 0) {
-    const resultPage = document.createElement('div')
-    resultPage.className = 'searchResultPage'
-    resultPage.innerHTML = 'No results found for query "' + query + '"'
-    searchResults.append(resultPage)
-    return
-  }
+		// Show message when results is empty
+		if (results.length === 0) {
+			const resultPage = document.createElement("div");
+			resultPage.className = "searchResultPage";
+			resultPage.innerHTML = `No results found for query ${query}`;
+			searchResults.append(resultPage);
+			return;
+		}
 
-  const arr = results[0].result
-  if (results.length > 1) {
-    arr.concat(results[1].result)
-  }
+		const arr = results[0].result;
+		if (results.length > 1) {
+			arr.concat(results[1].result);
+		}
 
-  arr.filter((element, index, self) =>
-    self.findIndex(e => e.id === element.id) === index)
+		arr.filter(
+			(element, index, self) =>
+				self.findIndex((e) => e.id === element.id) === index,
+		);
 
-  const instance = new Mark(document.getElementById('searchResults')!)
-  arr.forEach((result) => {
-    const resultPage = document.createElement('div')
-    resultPage.className = 'searchResultPage'
+		const instance = new Mark(searchResults);
+		arr.forEach((result) => {
+			const resultPage = document.createElement("div");
+			resultPage.className = "searchResultPage";
 
-    const resultTitle = document.createElement('a')
-    resultTitle.className = 'searchResultTitle'
-    resultTitle.href = result.doc.href
-    resultTitle.innerHTML = result.doc.title
-    resultPage.append(resultTitle)
+			const resultTitle = document.createElement("a");
+			resultTitle.className = "searchResultTitle";
+			resultTitle.href = result.doc.href;
+			resultTitle.innerHTML = result.doc.title;
+			resultPage.append(resultTitle);
 
-    const resultBody = document.createElement('div')
-    resultBody.className = 'searchResultBody'
-    const matchPos = result.doc.body?.indexOf(query) ?? -1
-    const bodyStartPos = matchPos - BODY_LENGTH / 2 > 0 ? matchPos - BODY_LENGTH / 2 : 0
-    resultBody.innerHTML = result.doc.body?.substr(bodyStartPos, BODY_LENGTH) ?? ''
-    resultPage.append(resultBody)
-    if (searchResults) {
-      searchResults.append(resultPage)
-    }
-    instance.mark(query)
-  })
+			const resultBody = document.createElement("div");
+			resultBody.className = "searchResultBody";
+			const matchPos = result.doc.body?.indexOf(query) ?? -1;
+			const bodyStartPos =
+				matchPos - BODY_LENGTH / 2 > 0 ? matchPos - BODY_LENGTH / 2 : 0;
+			resultBody.innerHTML =
+				result.doc.body?.substr(bodyStartPos, BODY_LENGTH) ?? "";
+			resultPage.append(resultBody);
+			if (searchResults) {
+				searchResults.append(resultPage);
+			}
+			instance.mark(query);
+		});
+	}
 }
 
 window.onload = function () {
-  init()
-}
+	init();
+};
