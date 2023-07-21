9547deae4c27b0156512d3c59926c4394b9f99f5
Luna <luna.dragon@suse.com>
2023-07-21 17:38:45 +0530

    TODO: add content warning support

--- static_src/src/js/comment.ts
+++ static_src/src/js/comment.ts
@@ -2,142 +2,155 @@ import DOMPurify from "dompurify";
 
 const HOST = "https://social.nullrequest.com/";
 const re =
-    /@[A-Za-z0-9_.]+@(((?!-))(xn--|_)?[a-z0-9-]{0,61}[a-z0-9]{1,1}\.)*(xn--)?([a-z0-9][a-z0-9\-]{0,60}|[a-z0-9-]{1,30}\.[a-z]{2,})/g;
+	/@[A-Za-z0-9_.]+@(((?!-))(xn--|_)?[a-z0-9-]{0,61}[a-z0-9]{1,1}\.)*(xn--)?([a-z0-9][a-z0-9\-]{0,60}|[a-z0-9-]{1,30}\.[a-z]{2,})/g;
 
 type Emoji = {
-    id: string;
-    url: string;
-    name: string;
+	id: string;
+	url: string;
+	name: string;
 };
 
 type User = {
-    username: string;
-    avatarUrl: string;
-    emojis: Emoji[];
+	username: string;
+    name: string;
+	avatarUrl: string;
+	emojis: Emoji[];
 };
 
 type UserDetails = {
-    host: string | null;
+	host: string | null;
 };
 
 type Reply = {
-    id: string;
-    text: string;
-    user: User;
-    emojis: Emoji[];
-    createdAt: string;
+	id: string;
+	text: string;
+	user: User;
+	emojis: Emoji[];
+	createdAt: string;
 };
 
-function escapeHtml(unsafe: string) {
-    return unsafe
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;");
+function escapeHtml(unsafe: string): string {
+	return unsafe
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&#039;");
 }
 
 function replace_emoji(input: string, emojis: Emoji[]): string {
-    let output = input;
-    emojis.forEach((emoji: Emoji) => {
-        output = output.replace(
-            `:${emoji.name}:`,
-            `<img src="${escapeHtml(emoji.url)}" alt="Emoji ${emoji.name
-            }" height="20" width="20" />`,
-        );
-    });
-    return output;
+	let output = input;
+	emojis.forEach((emoji: Emoji) => {
+		output = output.replace(
+			`:${emoji.name}:`,
+			`<img class="emoji" src="${escapeHtml(emoji.url)}" alt="Emoji ${
+				emoji.name
+			}" height="20" width="20" />`,
+		);
+	});
+	return output;
 }
 
-function init() {
-    const comments = document.getElementById("comments");
-    console.log("loading comments");
-    if (comments) {
-        const id = comments.getAttribute("data-noteid");
-        if (comments) {
-            comments.innerHTML = "Loading";
-            const data = {
-                noteId: id,
-                limit: 100,
-                depth: 100,
-            };
-            fetch("https://social.nullrequest.com/api/notes/children", {
-                method: "POST",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-            })
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data: Reply[]) {
-                    if (data && Array.isArray(data) && data.length > 0) {
-                        if (comments) {
-                            data.sort(
-                                (a, b) => Date.parse(a.createdAt) - Date.parse(b.createdAt),
-                            );
-                            data.reverse();
-                            comments.innerHTML = "";
-                            data.forEach(async function (reply) {
-                                reply.user.username = escapeHtml(reply.user.username);
+async function init() {
+	const comments = document.getElementById("comments");
+	console.log("loading comments");
+	if (comments) {
+		const id = comments.getAttribute("data-noteid");
+		const output: {
+			html: DocumentFragment;
+			date: string;
+		}[] = [];
 
-                                reply.text = escapeHtml(reply.text);
+		comments.innerHTML = "Loading";
+		const payload = {
+			noteId: id,
+			limit: 100,
+			depth: 100,
+		};
+		const resp = await fetch(
+			"https://social.nullrequest.com/api/notes/children",
+			{
+				method: "POST",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(payload),
+			},
+		);
 
-                                const array = [...reply.text.matchAll(re)];
+		const data: Reply[] = await resp.json();
+		if (data && Array.isArray(data) && data.length > 0) {
+			if (comments) {
+				if (data.length === 0) {
+					comments.innerHTML = "There are no comments";
+					return;
+				}
+				comments.innerHTML = "Loading Comments";
 
+				for (let i = 0; i < data.length; i++) {
+					const reply = data[i];
+					reply.user.username = escapeHtml(reply.user.username);
 
-                                reply.text = replace_emoji(reply.text, reply.emojis);
-                                reply.user.username = replace_emoji(reply.user.username, reply.user.emojis);
+					reply.text = escapeHtml(reply.text);
 
-                                if (array.length > 0) {
-                                    const fedi_username = array[0][0];
-                                    const split = fedi_username.split("@");
-                                    const username = split[1];
-                                    const domain = split[2];
-                                    const ref = `https://${domain}/@${username}`;
+                    reply.user.name = escapeHtml(reply.user.name);
 
-                                    reply.text = reply.text.replace(
-                                        array[0][0],
-                                        `<a class="MastodonUsername" href=${ref}>@${username}</a>`,
-                                    );
-                                }
+					const array = [...reply.text.matchAll(re)];
 
-                                const payload = {
-                                    query: reply.user.username,
-                                    offset: 0,
-                                    limit: 1,
-                                    origin: "combined",
-                                    detail: false,
-                                };
+					reply.text = replace_emoji(reply.text, reply.emojis);
+					reply.user.username = replace_emoji(
+						reply.user.username,
+						reply.user.emojis,
+					);
 
-                                const userdetails = await fetch(`${HOST}api/users/search`, {
-                                    method: "POST",
-                                    headers: {
-                                        Accept: "application/json",
-                                        "Content-Type": "application/json",
-                                    },
-                                    body: JSON.stringify(payload),
-                                });
-                                const json: UserDetails[] = await userdetails.json();
-                                const user = json[0];
-                                let host;
+					if (array.length > 0) {
+						const fedi_username = array[0][0];
+						const split = fedi_username.split("@");
+						const username = split[1];
+						const domain = split[2];
+						const ref = `https://${domain}/@${username}`;
 
-                                if (user.host) {
-                                    host = user.host;
-                                } else {
-                                    host = HOST.replace("https://", "").replace("/", "");
-                                }
+						reply.text = reply.text.replace(
+							array[0][0],
+							`<a class="MastodonUsername" href=${ref}>@${username}</a>`,
+						);
+					}
 
-                                const mastodonComment = `<div class="mastodon-comment">
+					const payload = {
+						query: reply.user.username,
+						offset: 0,
+						limit: 1,
+						origin: "combined",
+						detail: false,
+					};
+
+					const userdetails = await fetch(`${HOST}api/users/search`, {
+						method: "POST",
+						headers: {
+							Accept: "application/json",
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify(payload),
+					});
+					const json: UserDetails[] = await userdetails.json();
+					const user = json[0];
+					let host;
+
+					if (user.host) {
+						host = user.host;
+					} else {
+						host = HOST.replace("https://", "").replace("/", "");
+					}
+
+					const mastodonComment = `<div class="mastodon-comment">
 					   <div class="avatar">
-						 <img src="${escapeHtml(reply.user.avatarUrl)}" height=60 width=60 alt="">
+						 <img src="${escapeHtml(reply.user.avatarUrl)}" width=60 alt="">
 					   </div>
 					   <div class="content">
 						 <div class="author">
 						   <a href="https://${host}/@${reply.user.username}" rel="nofollow">
-							 <span>${reply.user.username}</span>
+							 <span>@${reply.user.username}</span>
 							 <span class="MastoHost">${host}</span>
 						   </a>
 						   <a class="date" href="${HOST}/notes/${reply.id}" rel="nofollow">
@@ -145,29 +158,29 @@ function init() {
 							</a>
 						 </div>
 						 <a class="date" href="${HOST}/notes/${reply.id}" rel="nofollow">
-						 <div class="mastodon-comment-content">${reply.text}</div> 
+						 <div class="mastodon-comment-content"><p>${reply.text}<p></div> 
 						 </a>
 					   </div>
 
 					 </div>`;
-                                if (comments) {
-                                    comments.appendChild(
-                                        DOMPurify.sanitize(mastodonComment, {
-                                            RETURN_DOM_FRAGMENT: true,
-                                        }),
-                                    );
-                                }
-                            });
-                        } else {
-                            const comments = document.getElementById("comments");
-                            if (comments) {
-                                comments.innerHTML = "<p>Not comments found</p>";
-                            }
-                        }
-                    }
-                });
-        }
-    }
+
+					output.push({
+						html: DOMPurify.sanitize(mastodonComment, {
+							RETURN_DOM_FRAGMENT: true,
+						}),
+						date: reply.createdAt,
+					});
+				}
+			} else {
+				return;
+			}
+		}
+		output.sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+		comments.innerHTML = "";
+		for (let i = 0; i < output.length; i++) {
+			comments.appendChild(output[i].html);
+		}
+	}
 }
 
 init();
